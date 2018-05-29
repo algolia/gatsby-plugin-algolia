43093eb3ec4ef6fb380a9b7b7c58ae0af84d0376
Phil <phil@ustwo.com>
2018-05-29 17:18:09 +0100

    Allow multiple indices

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,23 +3,25 @@ const chunk = require('lodash.chunk');
 
 exports.onPostBuild = function(
   { graphql },
-  { appId, apiKey, indexName, queries, chunkSize = 1000 }
+  { appId, apiKey, queries, chunkSize = 1000 }
 ) {
   const client = algoliasearch(appId, apiKey);
-  const index = client.initIndex(indexName);
 
-  const jobs = queries.map(async function doQuery({ query, transformer }) {
-    const result = await graphql(query);
-    const objects = transformer(result);
-    const chunks = chunk(objects, chunkSize);
+  const jobs = queries.map(
+    async function doQuery({ indexName, query, transformer }) {
+      const index = client.initIndex(indexName);
+      const result = await graphql(query);
+      const objects = transformer(result);
+      const chunks = chunk(objects, chunkSize);
 
-    const chunkJobs = chunks.map(async function(chunked) {
-      const { taskID } = await index.addObjects(chunked);
-      return index.waitTask(taskID);
-    });
+      const chunkJobs = chunks.map(async function(chunked) {
+        const { taskID } = await index.addObjects(chunked);
+        return index.waitTask(taskID);
+      });
 
-    return Promise.all(chunkJobs);
-  });
+      return Promise.all(chunkJobs);
+    }
+  );
 
   return Promise.all(jobs);
 };
