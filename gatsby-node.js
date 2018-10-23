4608cd56285b92f324f7e0e40eba2057135d47ee
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-23 09:14:09 +0100

    chore: re-save gatsby-node after pulling upstream prettierrc

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,23 @@
-const algoliasearch = require('algoliasearch')
-const chunk = require('lodash.chunk')
-const report = require('gatsby-cli/lib/reporter')
+const algoliasearch = require('algoliasearch');
+const chunk = require('lodash.chunk');
+const report = require('gatsby-cli/lib/reporter');
 
 /**
  * give back the same thing as this was called with.
  *
  * @param {any} obj what to keep the same
  */
-const identity = obj => obj
+const identity = obj => obj;
 
 exports.onPostBuild = async function(
   { graphql },
   { appId, apiKey, queries, indexName: mainIndexName, chunkSize = 1000 }
 ) {
-  const activity = report.activityTimer(`index to Algolia`)
-  activity.start()
-  const client = algoliasearch(appId, apiKey)
+  const activity = report.activityTimer(`index to Algolia`);
+  activity.start();
+  const client = algoliasearch(appId, apiKey);
 
-  setStatus(activity, `${queries.length} queries to index`)
+  setStatus(activity, `${queries.length} queries to index`);
 
   const jobs = queries.map(async function doQuery(
     { indexName = mainIndexName, query, transformer = identity, settings },
@@ -26,52 +26,52 @@ exports.onPostBuild = async function(
     if (!query) {
       report.panic(
         `failed to index to Algolia. You did not give "query" to this query`
-      )
+      );
     }
-    const index = client.initIndex(indexName)
-    const mainIndexExists = await indexExists(index)
-    const tmpIndex = client.initIndex(`${indexName}_tmp`)
-    const indexToUse = mainIndexExists ? tmpIndex : index
+    const index = client.initIndex(indexName);
+    const mainIndexExists = await indexExists(index);
+    const tmpIndex = client.initIndex(`${indexName}_tmp`);
+    const indexToUse = mainIndexExists ? tmpIndex : index;
 
     if (mainIndexExists) {
-      setStatus(activity, `query ${i}: copying existing index`)
-      await scopedCopyIndex(client, index, tmpIndex)
+      setStatus(activity, `query ${i}: copying existing index`);
+      await scopedCopyIndex(client, index, tmpIndex);
     }
 
-    setStatus(activity, `query ${i}: executing query`)
-    const result = await graphql(query)
+    setStatus(activity, `query ${i}: executing query`);
+    const result = await graphql(query);
     if (result.errors) {
-      report.panic(`failed to index to Algolia`, result.errors)
+      report.panic(`failed to index to Algolia`, result.errors);
     }
-    const objects = transformer(result)
-    const chunks = chunk(objects, chunkSize)
+    const objects = transformer(result);
+    const chunks = chunk(objects, chunkSize);
 
-    setStatus(activity, `query ${i}: splitting in ${chunks.length} jobs`)
+    setStatus(activity, `query ${i}: splitting in ${chunks.length} jobs`);
 
     const chunkJobs = chunks.map(async function(chunked) {
-      const { taskID } = await indexToUse.addObjects(chunked)
-      return indexToUse.waitTask(taskID)
-    })
+      const { taskID } = await indexToUse.addObjects(chunked);
+      return indexToUse.waitTask(taskID);
+    });
 
-    await Promise.all(chunkJobs)
+    await Promise.all(chunkJobs);
 
     tmpIndex.setSettings(settings, err => {
-      if (err) throw err
-    })
+      if (err) throw err;
+    });
 
     if (mainIndexExists) {
-      setStatus(activity, `query ${i}: moving copied index to main index`)
-      return moveIndex(client, tmpIndex, index)
+      setStatus(activity, `query ${i}: moving copied index to main index`);
+      return moveIndex(client, tmpIndex, index);
     }
-  })
+  });
 
   try {
-    await Promise.all(jobs)
+    await Promise.all(jobs);
   } catch (err) {
-    report.panic(`failed to index to Algolia`, err)
+    report.panic(`failed to index to Algolia`, err);
   }
-  activity.end()
-}
+  activity.end();
+};
 
 /**
  * Copy the settings, synonyms, and rules of the source index to the target index
@@ -85,8 +85,8 @@ async function scopedCopyIndex(client, sourceIndex, targetIndex) {
     sourceIndex.indexName,
     targetIndex.indexName,
     ['settings', 'synonyms', 'rules']
-  )
-  return targetIndex.waitTask(taskID)
+  );
+  return targetIndex.waitTask(taskID);
 }
 
 /**
@@ -100,8 +100,8 @@ async function moveIndex(client, sourceIndex, targetIndex) {
   const { taskID } = await client.moveIndex(
     sourceIndex.indexName,
     targetIndex.indexName
-  )
-  return targetIndex.waitTask(taskID)
+  );
+  return targetIndex.waitTask(taskID);
 }
 
 /**
@@ -111,10 +111,10 @@ async function moveIndex(client, sourceIndex, targetIndex) {
  */
 async function indexExists(index) {
   try {
-    const { nbHits } = await index.search()
-    return nbHits > 0
+    const { nbHits } = await index.search();
+    return nbHits > 0;
   } catch (e) {
-    return false
+    return false;
   }
 }
 
@@ -126,8 +126,8 @@ async function indexExists(index) {
  */
 function setStatus(activity, status) {
   if (activity && activity.setStatus) {
-    activity.setStatus(status)
+    activity.setStatus(status);
   } else {
-    console.log('Algolia:', status)
+    console.log('Algolia:', status);
   }
 }
