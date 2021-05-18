bfa71808bf4fda246da81d8fc90ca71be91a8035
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-05-18 04:48:07 -0300

    feat(core): Migrate to algoliasearch@4.x (#135)
    * feat: migrate to algoliasearch 4.x
    
    * chore: wait for tasks to complete

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,21 +8,28 @@ const deepEqual = require('deep-equal');
  * @param {AlgoliaIndex} index eg. client.initIndex('your_index_name');
  * @param {Array<String>} attributesToRetrieve eg. ['modified', 'slug']
  */
-function fetchAlgoliaObjects(index, attributesToRetrieve = ['modified']) {
-  return new Promise((resolve, reject) => {
-    const browser = index.browseAll('', { attributesToRetrieve });
-    const hits = {};
+function fetchAlgoliaObjects(
+  index,
+  attributesToRetrieve = ['modified'],
+  reporter
+) {
+  const hits = {};
 
-    browser.on('result', content => {
-      if (Array.isArray(content.hits)) {
-        content.hits.forEach(hit => {
-          hits[hit.objectID] = hit;
-        });
-      }
-    });
-    browser.on('end', () => resolve(hits));
-    browser.on('error', err => reject(err));
-  });
+  return index
+    .browseObjects({
+      batch: batch => {
+        if (Array.isArray(batch)) {
+          batch.forEach(hit => {
+            hits[hit.objectID] = hit;
+          });
+        }
+      },
+      attributesToRetrieve,
+    })
+    .then(() => hits)
+    .catch(err =>
+      reporter.panicOnBuild('failed while getting indexed objects', err)
+    );
 }
 
 exports.onPostBuild = async function ({ graphql, reporter }, config) {
@@ -44,7 +51,13 @@ exports.onPostBuild = async function ({ graphql, reporter }, config) {
     return;
   }
 
-  const client = algoliasearch(appId, apiKey, { timeout: 30_000 });
+  const client = algoliasearch(appId, apiKey, {
+    timeouts: {
+      connect: 1,
+      read: 30,
+      write: 30,
+    },
+  });
 
   activity.setStatus(`${queries.length} queries to index`);
 
@@ -164,7 +177,8 @@ async function runIndexQueries(
     // get all indexed objects matching all matched fields
     const indexedObjects = await fetchAlgoliaObjects(
       indexToUse,
-      allMatchFields
+      allMatchFields,
+      reporter
     );
 
     // iterate over each query
@@ -237,8 +251,7 @@ async function runIndexQueries(
 
     /* Add changed / new objects */
     const chunkJobs = chunks.map(async function (chunked) {
-      const { taskID } = await indexToUse.addObjects(chunked);
-      return indexToUse.waitTask(taskID);
+      await indexToUse.saveObjects(chunked);
     });
 
     await Promise.all(chunkJobs);
@@ -251,8 +264,7 @@ async function runIndexQueries(
       `Found ${objectsToRemove.length} stale objects; removing...`
     );
 
-    const { taskID } = await indexToUse.deleteObjects(objectsToRemove);
-    await indexToUse.waitTask(taskID);
+    await indexToUse.deleteObjects(objectsToRemove).wait();
   }
 
   // defer to first query for index settings
@@ -267,11 +279,11 @@ async function runIndexQueries(
     reporter,
   });
 
-  const { taskID } = await indexToUse.setSettings(settingsToApply, {
-    forwardToReplicas,
-  });
-
-  await indexToUse.waitTask(taskID);
+  await indexToUse
+    .setSettings(settingsToApply, {
+      forwardToReplicas,
+    })
+    .wait();
 
   if (indexToUse === tempIndex) {
     await moveIndex(client, indexToUse, index);
@@ -288,11 +300,7 @@ async function runIndexQueries(
  * @return {Promise}
  */
 async function moveIndex(client, sourceIndex, targetIndex) {
-  const { taskID } = await client.moveIndex(
-    sourceIndex.indexName,
-    targetIndex.indexName
-  );
-  return targetIndex.waitTask(taskID);
+  return client.moveIndex(sourceIndex.indexName, targetIndex.indexName).wait();
 }
 
 /**
@@ -418,7 +426,6 @@ function getAllMatchFields(queries, mainMatchFields = []) {
 }
 
 async function createIndex(index) {
-  const { taskID } = await index.setSettings({});
-  await index.waitTask(taskID);
+  await index.setSettings({}).wait();
   return index;
 }
